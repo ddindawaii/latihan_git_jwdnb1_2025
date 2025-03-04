7d10e5fe7f49f0b888ccb48285151af52a572316
Dinda Aprilia Dedsa Putri <ddindawaii@gmail.com>
2025-03-04 09:23:12 +0700

    menambahkan halaman belajar git

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll("nav ul li a").forEach(link => {
+    link.addEventListener("click", function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute("href").substring(1);
+        document.getElementById(targetId).scrollIntoView({ behavior: "smooth" });
+    });
+});
